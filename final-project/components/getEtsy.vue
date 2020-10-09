5142111397d6213335b4786b7302a03a66832995
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-10-08 19:22:07 -0500

    more aesthetic editing

--- final-project/components/getEtsy.vue
+++ final-project/components/getEtsy.vue
@@ -8,7 +8,6 @@
 </script>
 
 <template>
-
   <div id="get-etsy">
     <b-card title="Listing" sub-title="Favorites">
       <b-card-text>
@@ -19,16 +18,3 @@
       <a :href="`https://www.etsy.com/listing/${listing.listing_id}`">View Listing</a>
     </b-card>
   </div>
-  <!-- <div id="get-etsy"> -->
-    <!-- v-if loops through favorited listings and displays ones that have active state -->
-    <!-- <section class="card" v-if="listing.listing_state == 'active'"> -->
-      <!-- <h2 class="card__title">{{ listing.name }}</h2> -->
-      <!-- USE AS DEBUGGING TOOL <p>{{ listing }}</p> -->
-      <!-- <a :href="`https://www.etsy.com/listing/${listing.listing_id}`">CLICK</a>
-
-      <p class="card__copy">{{ listing.listing_state }} {{ listing.create_date }}</p> -->
-
-      <!--print out a link if it's still active, or date listing was saved -->
-    <!-- </section>
-    </div> -->
-</template>
--- final-project/pages/home.vue
+++ final-project/pages/home.vue
@@ -7,8 +7,10 @@ export default {
       showJumbotron: true,
       soon: 'Interact with my site to learn more about my professional achievements and some interesting info about me.',
       // subhead: 'Recently Updated',
-      content: 'Experienced community and hospitality management strategist with expertise in community outreach, event coordination, professional writing and editing, and corporate marketing strategies. Works well with a team in an open concept office, individually as a self-starter, and resilient to workplace changes. Professional growth-seeker, strategic thinker, and unabashed user of Oxford commas. I come from a blended professional background in community engagement, marketing, and event planning. My educational background began in communications and journalism, where I began as a beat reporter and worked up to a section editor. During that time, I started to gain marketing experience at a small National Historic landmark. As I emerged from these positions, I learned early in my professional beginnings two important things: one, that I would be willing to do what it takes as a team member to create the content we needed (e.g. dress up in historic clothing in the summer in order to create original content for marketing materials), and two, how much I prefer the use of an Oxford comma over the traditional journalistic AP style.  I have shifted focus to continue to grow professionally by diving headfirst into the technology sector. I am currently pursuing a Certificate in Front-End Web Development online at the University of Washington. This program plays to my strengths, continuing to hone my attention to detail; one misplaced punctuation mark and a website can literally grind to a halt. Though it is a challenging pursuit, it is pushing me to grow professionally, inspiring me to rethink how I learn, and showing me beauty in a place that I previously thought was limited to logic.  I would love to bring these experiences to Wirespring and help launch their social media presence and contribute to industry blog content. Digging into research to find nuggets of information that clients are looking to leverage to boost sales and build networks of smart, connected devices with Wirespring’s application enablement platform and cloud services, and deliver messages from a distance with environmentally-friendly, daylight readable displays.',
-      notes: 'My short term professional goal is to become a better a11y (ally) in implementing web accessibility. There are very easy things that businesses can do to open digital accessibility to more of their audience, and these changes aren’t solely reliant on the web development segment of their teams. As web development professionals, we can collaborate with our clients to begin to design products to better suit these needs, and in very simple ways, such as making sure color palettes include contrast ratios that maximize readability. I also plan to become more proficient in ES6+ syntax.'
+      content: 'Experienced community and hospitality management strategist with expertise in community outreach, event coordination, professional writing and editing, and corporate marketing strategies. Works well with a team in an open concept office, individually as a self-starter, and resilient to workplace changes',
+      contentTwo: 'Professional growth-seeker, strategic thinker, and unabashed user of Oxford commas',
+      notes: 'Continue to be a better a11y (ally) in implementing web accessibility',
+      notesTwo: 'Foster self-leaerning in imporving in ES6+ syntax'
     }
   }
 }
@@ -29,13 +31,15 @@ export default {
         <!-- v-on:click enables button to change content if clicked, otherwise content is shown due to != making it false-->
         <button v-on:click="showJumbotron = !showJumbotron">Show</button>
       </div>
-      <div>
+      <div class="">
         <h2 class="display-4">Merissa O'Reilly</h2>
         <p class="lead">{{ content }}</p>
+        <p class="lead">{{ contentTwo }}</p>
       </div>
       <div>
-        <h2 class="display-4">Short Term Goal</h2>
+        <h2 class="display-4">Short Term Goals</h2>
         <p class="lead">{{ notes }}</p>
+        <p class="lead">{{ notesTwo }}</p>
       </div>
   </div>
 </template>
